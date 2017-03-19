f414e1194ef0222710411d83353016f6a8d5e09d
Ajay Divakaran <ajay_886@hotmail.com>
2017-03-19 17:33:12 +0530

    Completed KafkaListener logging

--- core/egov-notification-sms/src/main/java/org/egov/web/notification/sms/consumer/SmsNotificationListener.java
+++ core/egov-notification-sms/src/main/java/org/egov/web/notification/sms/consumer/SmsNotificationListener.java
@@ -5,7 +5,6 @@ import org.egov.web.notification.sms.models.RequestContext;
 import org.egov.web.notification.sms.services.SMSService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -23,7 +22,7 @@ public class SmsNotificationListener {
     @KafkaListener(id = "${kafka.topics.notification.sms.id}",
             topics = "${kafka.topics.notification.sms.name}",
             group = "${kafka.topics.notification.sms.group}")
-    public void process(@Payload SMSRequest request) {
+    public void process(SMSRequest request) {
         RequestContext.setId(UUID.randomUUID().toString());
         smsService.sendSMS(request.toDomain());
     }
