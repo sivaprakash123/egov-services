00dbbfa1dd02195394315b9b287d73e798874d9e
Ajay Divakaran <ajay_886@hotmail.com>
2017-05-11 12:01:11 +0530

    temp commit

---
+++ pgr/pgr-rest/src/main/java/org/egov/pgrrest/common/entity/ServiceDefinition.java
@@ -0,0 +1,12 @@
+package org.egov.pgrrest.common.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+public class ServiceDefinition {
+}
