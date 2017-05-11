aefb654073544de8ace3978ed7ff6cdc767d6604
Ajay Divakaran <ajay_886@hotmail.com>
2017-05-11 11:44:02 +0530

    Added sample definition and dev/qa migration folders

--- pgr/pgr-rest/src/main/resources/db/migration/ddl/V20170510145816__create_service_definition_table.sql
+++ pgr/pgr-rest/src/main/resources/db/migration/ddl/V20170510145816__create_service_definition_table.sql
@@ -1,7 +1,11 @@
 CREATE TABLE service_definition (
     code character varying(20),
     tenantid character varying(256) NOT NULL,
-    version bigint DEFAULT 0
+    version bigint DEFAULT 0,
+    createddate timestamp NOT NULL,
+    lastmodifieddate timestamp ,
+    createdby bigint NOT NULL,
+    lastmodifiedby bigint
 );
 
 ALTER TABLE service_definition ADD CONSTRAINT service_definition_pkey
@@ -15,26 +19,36 @@ CREATE TABLE attribute_definition (
     datatypedescription VARCHAR(200),
     ordernum INTEGER,
     description VARCHAR(300),
-    service_code VARCHAR (256) NOT NULL,
-    tenantid character varying(256) NOT NULL
+    servicecode VARCHAR (256) NOT NULL,
+    tenantid character varying(256) NOT NULL,
+    version bigint DEFAULT 0,
+    createddate timestamp NOT NULL,
+    lastmodifieddate timestamp ,
+    createdby bigint NOT NULL,
+    lastmodifiedby bigint
 );
 
 ALTER TABLE attribute_definition ADD CONSTRAINT attribute_definition_pkey
-PRIMARY KEY(service_code, code, tenantid);
+PRIMARY KEY(servicecode, code, tenantid);
 
 ALTER TABLE attribute_definition ADD CONSTRAINT attribute_definition_fkey
-FOREIGN KEY (service_code, tenantid) REFERENCES service_definition (code, tenantid);
+FOREIGN KEY (servicecode, tenantid) REFERENCES service_definition (code, tenantid);
 
 CREATE TABLE values_definition (
-    service_code VARCHAR (256) NOT NULL,
-    attribute_code VARCHAR(50) NOT NULL,
+    servicecode VARCHAR (256) NOT NULL,
+    attributecode VARCHAR(50) NOT NULL,
     key VARCHAR(50) NOT NULL,
     name VARCHAR(100) NOT NULL,
-    tenantid character varying(256) NOT NULL
+    tenantid character varying(256) NOT NULL,
+    version bigint DEFAULT 0,
+    createddate timestamp NOT NULL,
+    lastmodifieddate timestamp ,
+    createdby bigint NOT NULL,
+    lastmodifiedby bigint
 );
 
 ALTER TABLE values_definition ADD CONSTRAINT values_definition_pkey
-PRIMARY KEY(attribute_code, key, tenantid);
+PRIMARY KEY(attributecode, key, tenantid);
 
 ALTER TABLE values_definition ADD CONSTRAINT values_definition_attribute_definition_fkey
-FOREIGN KEY (service_code, attribute_code, tenantid) REFERENCES attribute_definition (service_code, code, tenantid);
+FOREIGN KEY (servicecode, attributecode, tenantid) REFERENCES attribute_definition (servicecode, code, tenantid);
