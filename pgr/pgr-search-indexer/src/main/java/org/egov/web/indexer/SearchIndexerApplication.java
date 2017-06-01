3dbec4a07b0efdc994bcc454874612f38a12a3bc
Ajay Divakaran <ajay_886@hotmail.com>
2017-06-01 11:13:40 +0530

    pgr-search-indexer move to ES Transport client

--- pgr/pgr-search-indexer/src/main/java/org/egov/web/indexer/SearchIndexerApplication.java
+++ pgr/pgr-search-indexer/src/main/java/org/egov/web/indexer/SearchIndexerApplication.java
@@ -3,12 +3,20 @@ package org.egov.web.indexer;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.egov.tracer.config.TracerConfiguration;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
+import javax.annotation.PostConstruct;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.TimeZone;
 
 @SpringBootApplication
@@ -16,6 +24,30 @@ import java.util.TimeZone;
 public class SearchIndexerApplication {
 
     private static final String IST = "Asia/Calcutta";
+    private static final String CLUSTER_NAME = "cluster.name";
+
+    private TransportClient client;
+
+    @Value("${es.host}")
+    private String elasticSearchHost;
+
+    @Value("${es.transport.port}")
+    private Integer elasticSearchTransportPort;
+
+    @Value("${es.cluster.name}")
+    private String elasticSearchClusterName;
+
+    @PostConstruct
+    public void init() throws UnknownHostException {
+        Settings settings = Settings.builder()
+            .put(CLUSTER_NAME, elasticSearchClusterName)
+            .build();
+        final InetAddress esAddress = InetAddress.getByName(elasticSearchHost);
+        final InetSocketTransportAddress transportAddress =
+            new InetSocketTransportAddress(esAddress, elasticSearchTransportPort);
+        client = new PreBuiltTransportClient(settings)
+            .addTransportAddress(transportAddress);
+    }
 
     @Bean
     public ObjectMapper getObjectMapper() {
@@ -29,6 +61,11 @@ public class SearchIndexerApplication {
         MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
         converter.setObjectMapper(objectMapper);
         return converter;
+    }
+
+    @Bean
+    public TransportClient getTransportClient() {
+        return client;
     }
 
     public static void main(String[] args) {
